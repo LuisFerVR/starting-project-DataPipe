9b1718f738dfdd735dfc6ae48640abaaf4b9236f
LuisFerVR <luis.ruiz@ex.gerdau.com>
2024-09-27 16:40:54 -0600

    Tuberías personalizadas configurables.

--- src/app/temperature.pipe.ts
+++ src/app/temperature.pipe.ts
@@ -7,14 +7,35 @@ import { Pipe, PipeTransform } from "@angular/core";
 })
 
 export class TemperaturePipe implements PipeTransform{
-    transform(value:string | number) {
+    transform(value:string | number | null, inputType:'cel'|'fah', outputType?:'cel'|'fah') {
+        let outputTemp:number;
         let val:number;
+        let symbol: '°C' | '°F';
+        
+        if(!value){
+            return value;
+        }
+
         if(typeof value === 'string'){
             val = parseFloat(value);
         }else{
             val = value;
         }
-        const outputTemp = val *(9/5) + 32;
-        return `${outputTemp}°F`;
+        
+        if(inputType === 'cel' && outputType === 'fah'){
+            outputTemp = val *(9/5) + 32;
+        }else if(inputType==='fah' && outputType === 'cel'){
+            outputTemp = (val-32) * (5/9)
+        }else{
+            outputTemp = val;
+        }
+
+        if(!outputType){
+            symbol = inputType === 'cel' ? '°C' : '°F';
+        }else{
+            symbol = outputType === 'cel' ? '°C' : '°F';
+        }
+
+        return `${outputTemp.toFixed(2)} ${symbol}`;
     }
 }
